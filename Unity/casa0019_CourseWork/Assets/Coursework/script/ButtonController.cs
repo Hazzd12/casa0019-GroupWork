3598aa5832e9cf0128e6e44be923b64de6f739e1
ExtrAzzzz <ucfnuax@ucl.ac.uk>
2023-12-13 05:21:30 +0800

    Update image tracker and UI

--- Unity/casa0019_CourseWork/Assets/Coursework/script/ButtonController.cs
+++ Unity/casa0019_CourseWork/Assets/Coursework/script/ButtonController.cs
@@ -4,19 +4,16 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour
 {
     public Canvas canvas; // 引用Canvas
-    public Button toggleButton; // 引用Button
-
     void Start()
     {
-        // 添加按钮点击事件监听器
-        toggleButton.onClick.AddListener(ToggleCanvas);
-        
+        Debug.Log("Start method is called.");
         // 默认情况下隐藏Canvas
         canvas.gameObject.SetActive(false);
     }
 
-    void ToggleCanvas()
+    public void ToggleCanvas()
     {
+        Debug.Log("TTTTTTTTTTTTTTTTTTTT");
         // 切换Canvas的显示状态
         canvas.gameObject.SetActive(!canvas.gameObject.activeSelf);
     }
